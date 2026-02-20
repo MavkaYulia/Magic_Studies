117a4f2750b3a37ec020ca60eb0e3ac4c4cd949f
Yulia <mavkayulia@gmail.com>
2026-02-20 19:21:06 +0200

    add BottomNavigationBar

--- app/src/main/java/com/mavka/magicstudiesapp/presentation/theme/MainActivity.kt
+++ app/src/main/java/com/mavka/magicstudiesapp/presentation/theme/MainActivity.kt
@@ -4,18 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode.Companion.Screen
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.createGraph
 import com.mavka.magicstudiesapp.presentation.navigation.AppNavigation
 import com.mavka.magicstudiesapp.presentation.navigation.BottomNavigationBar
-import com.mavka.magicstudiesapp.presentation.theme.ui.MagicStudiesAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
