342c010fac3097a24895e0c19437cb9e3834061b
Yulia <mavkayulia@gmail.com>
2026-03-02 01:35:05 +0200

    update UI

--- app/src/main/java/com/mavka/magicstudiesapp/presentation/navigation/NavigationItem.kt
+++ app/src/main/java/com/mavka/magicstudiesapp/presentation/navigation/NavigationItem.kt
@@ -1,11 +1,11 @@
 package com.mavka.magicstudiesapp.presentation.navigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.material.icons.filled.MailOutline
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.Castle
+import androidx.compose.material.icons.filled.Explore
+import androidx.compose.material.icons.filled.HourglassEmpty
+import androidx.compose.material.icons.filled.LibraryMusic
+import androidx.compose.material.icons.filled.Style
 import androidx.compose.ui.graphics.vector.ImageVector
 
 data class NavigationItem(
@@ -18,27 +18,27 @@ val navigationItems = listOf(
 
     NavigationItem(
         title = "Atlas",
-        icon = Icons.Default.LocationOn,
+        icon = Icons.Default.Explore,
         route = Screen.Atlas.route
     ),
     NavigationItem(
         title = "Quests",
-        icon = Icons.Default.MailOutline,
+        icon = Icons.Default.Style,
         route = Screen.Quests.route
     ),
     NavigationItem(
         title = "CentralHall",
-        icon = Icons.Default.Home,
+        icon = Icons.Default.Castle,
         route = Screen.CentralHall.route
     ),
     NavigationItem(
         title = "Timer",
-        icon = Icons.Default.Notifications,
+        icon = Icons.Default.HourglassEmpty,
         route = Screen.Timer.route
     ),
     NavigationItem(
         title = "Music",
-        icon = Icons.Default.Star,
+        icon = Icons.Default.LibraryMusic,
         route = Screen.Music.route
     ),
 )
--- app/src/main/java/com/mavka/magicstudiesapp/presentation/theme/ui/Theme.kt
+++ app/src/main/java/com/mavka/magicstudiesapp/presentation/theme/ui/Theme.kt
@@ -1,41 +1,12 @@
 package com.mavka.magicstudiesapp.presentation.theme.ui
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
-
-
-private val DarkColorScheme = darkColorScheme(
-    primary = MagicColor.WaxSealRed,
-    secondary = MagicColor.ForestGreen,
-    tertiary = MagicColor.FadedGold
-)
-
-/*
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-*/
 
 @Composable
 fun MagicStudiesAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    /*  val colorScheme = when {
-          dynamicColor -> {
-              val context = LocalContext.current
-              if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-          }
-
-          darkTheme -> DarkColorScheme
-          else -> LightColorScheme
-      }*/
-
     MaterialTheme(
         colorScheme = MagicMaterialColor,
         typography = MagicMaterialTypography,
