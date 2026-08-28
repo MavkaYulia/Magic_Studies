40a0a7ef0d5941ab74bbf569953c1a3d82bf3941
Yuliia <mavkayulia@gmail.com>
2026-08-28 23:13:05 +0300

    optimize repo, add tabs to quest directory, fix the cards with subjects, rework idea of adding subquests

---
+++ app/src/main/java/com/mavka/magicstudiesapp/presentation/screens/quests/QuestContainer.kt
@@ -0,0 +1,61 @@
+package com.mavka.magicstudiesapp.presentation.screens.quests
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Book
+import androidx.compose.material.icons.filled.QueryStats
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import kotlinx.coroutines.launch
+
+data class QuestTab(
+    val title: String,
+    val icon: ImageVector
+)
+
+@Composable
+fun QuestContainer() {
+    val tabs =
+        listOf(QuestTab("Quests", Icons.Default.Book), QuestTab("Stats", Icons.Default.QueryStats))
+    val pagerState = rememberPagerState { tabs.size }
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(modifier = Modifier.wrapContentSize()) {
+        TabRow(selectedTabIndex = pagerState.currentPage) {
+            tabs.forEachIndexed { index, tab ->
+                Tab(
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        coroutineScope.launch { pagerState.animateScrollToPage(index) }
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = tab.icon,
+                            contentDescription = tab.title
+                        )
+                    },
+                    text = { Text(text = tab.title) }
+                )
+            }
+        }
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.weight(1f)
+        ) { page ->
+            when (page) {
+                0 -> QuestsScreen()
+                1 -> StatsScreen()
+            }
+        }
+    }
+}
